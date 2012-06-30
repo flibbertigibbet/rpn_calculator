9923f31fa2781cb67a0e62bce54027524ef79e9d
Kathryn Killebrew <banderkat@gmail.com>
2012-06-30 05:49:46 -0500

    Completed walkthrough for RPN calculator for lesson 2.

---
+++ Calculator/CalculatorBrain.h
@@ -0,0 +1,16 @@
+//
+//  CalculatorBrain.h
+//  Calculator
+//
+//  Created by kat on 6/30/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CalculatorBrain : NSObject
+
+- (void) pushOperand:(double)operand;
+- (double) performOperation:(NSString *)operation;
+
+@end
--- Calculator/CalculatorViewController.m
+++ Calculator/CalculatorViewController.m
@@ -7,14 +7,23 @@
 //
 
 #import "CalculatorViewController.h"
+#import "CalculatorBrain.h"
 
 @interface CalculatorViewController ()
 @property (nonatomic) BOOL userIsInTheMiddleOfEnteringANumber;
+@property (nonatomic, strong) CalculatorBrain *brain;
 @end
 
 @implementation CalculatorViewController
 @synthesize display;
 @synthesize userIsInTheMiddleOfEnteringANumber;
+@synthesize brain = _brain;
+
+- (CalculatorBrain *)brain
+{
+    if (!_brain) _brain = [[CalculatorBrain alloc] init];
+    return _brain;
+}
 
 - (IBAction)digitPressed:(UIButton *)sender {
     NSString *digit = sender.currentTitle;
@@ -28,10 +37,21 @@
         self.userIsInTheMiddleOfEnteringANumber = YES;
     }
 }
-- (IBAction)enterPressed {
+- (IBAction)enterPressed
+{
+    [self.brain pushOperand:[self.display.text doubleValue]];
+    self.userIsInTheMiddleOfEnteringANumber = NO;
 }
 
-- (IBAction)operationPressed:(UIButton *)sender {
+- (IBAction)operationPressed:(UIButton *)sender
+{
+    if (self.userIsInTheMiddleOfEnteringANumber) {
+        [self enterPressed];
+    }
+    
+    NSString *operation = sender.currentTitle;
+    double result = [self.brain performOperation:operation];
+    self.display.text = [NSString stringWithFormat:@"%g", result];
 }
 
 @end
