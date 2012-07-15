b456cba894922ed9d8f6749c837441443a105494
Kathryn Killebrew <banderkat@gmail.com>
2012-07-14 22:40:44 -0500

    Add graph view and controller.

---
+++ Calculator/GraphView.h
@@ -0,0 +1,13 @@
+//
+//  GraphView.h
+//  Calculator
+//
+//  Created by Kathryn Killebrew on 7/14/12.
+//  Copyright (c) 2012 Banderkat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GraphView : UIView
+
+@end
---
+++ Calculator/GraphViewController.m
@@ -0,0 +1,33 @@
+//
+//  GraphViewController.m
+//  Calculator
+//
+//  Created by Kathryn Killebrew on 7/14/12.
+//  Copyright (c) 2012 Banderkat. All rights reserved.
+//
+
+#import "GraphViewController.h"
+
+@interface GraphViewController ()
+@property (nonatomic) id myProgram;
+@end
+
+@implementation GraphViewController
+@synthesize myProgram = _myProgram;
+
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
