92aa331a3e46a207538e1d5082cb5686f91a79c1
Kathryn Killebrew <banderkat@gmail.com>
2012-07-20 04:03:19 -0500

    Fix scaling.
    Correct axes drawing for corner.

--- Calculator/GraphView.m
+++ Calculator/GraphView.m
@@ -42,29 +42,51 @@
     origin = [self.dataSource getOrigin];
     if (!origin.x) origin = self.center;
     
+    NSLog(@"%@%g", @"scale ", scale);
+    
     CGContextBeginPath(context);
     
     [AxesDrawer drawAxesInRect:bounds originAtPoint:origin scale:scale];
     
     CGFloat xOffeset = origin.x;
     CGFloat yOffset = origin.y;
-    CGFloat myWidth = (bounds.size.width + xOffeset) * scale;
-    
-    endPoint.x = -xOffeset/scale;
-    endPoint.y = yOffset-scale*[[self.dataSource getY:endPoint.x - xOffeset] floatValue];
+    CGFloat myWidth = bounds.size.width;
     
     CGFloat pixelIncrement = 1.0/self.contentScaleFactor;
     
-    for (CGFloat x=-myWidth; x <= myWidth; x += pixelIncrement) {
+    /*
+    NSLog(@"%@%g%@%g", @"graph origin x ", endPoint.x, 
+          @"  graph origin y ", endPoint.y);
+    NSLog(@"%@%g%@%g", @"start x ", -xOffeset / scale, @"  start y ", [[self.dataSource getY:(-xOffeset / scale)] floatValue]);
+    CGPoint graphEnd;
+    graphEnd.x = (myWidth - xOffeset) / scale;
+    graphEnd.y = [[self.dataSource getY:graphEnd.x] floatValue];
+    NSLog(@"%@%g%@%g", @"end graph x ", graphEnd.x, 
+          @" end graph y ", graphEnd.y);
+    */
+    
+    endPoint.x = 0;
+    endPoint.y = [[self.dataSource getY:(-xOffeset / scale)] floatValue];
+    
+    for (CGFloat x=0; x <= myWidth; x+=pixelIncrement) {
         startPoint.x = endPoint.x;
         startPoint.y = endPoint.y;
-        endPoint.x = (x - xOffeset) / scale;
-        endPoint.y = yOffset-scale*[[self.dataSource getY:endPoint.x - xOffeset] floatValue];
+        
+        //graphPoint.x = (x - xOffeset) / scale;
+        //graphPoint.y = [[self.dataSource getY:graphPoint.x] floatValue];
+        //NSLog(@"%@%g%@%g", @"graph x ", graphPoint.x, @"  graph y ", graphPoint.y);
+        //endPoint.y = yOffset - (graphPoint.y * scale);
+        
+        endPoint.x = x;
+        endPoint.y = yOffset - ([[self.dataSource getY:(x - xOffeset) / scale] floatValue] * scale);
+        
     
         CGContextMoveToPoint(context, startPoint.x, startPoint.y);
         CGContextAddLineToPoint(context, endPoint.x, endPoint.y);
     }
-    
+
+       
+    CGContextStrokePath(context);
     CGContextStrokePath(context);
 }
 
